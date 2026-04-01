9232f3cf608041814cebafe98beddeb937f72088
Yoga Ulil <yoga.ulil@aray.ma>
2026-04-01 12:57:30 +0700

    enhance

--- src/format/excursion.contentful.format.js
+++ src/format/excursion.contentful.format.js
@@ -1,7 +1,8 @@
 const formatExcursionTripList = (item) => {
   return {
     id: item.sys.id,
-    title: item.fields.title,
+    title: item.fields.title + " Day Trip",
+    rawTitle: item.fields.title,
     slug: item.fields.slug,
     heroImage: item.fields.heroImage?.fields?.file?.url || null,
     summary: item.fields.summary
@@ -9,15 +10,31 @@ const formatExcursionTripList = (item) => {
 }
 
 const formatExcursionTripDetail = (item) => {
+  const allPhotos =
+    item.fields.photos?.map((p) => p.fields?.file?.url).filter(Boolean) || []
+
+  const characterImages =
+    item.fields.characterImages?.map((p) => p.fields?.file?.url).filter(Boolean) || []
+
   return {
     id: item.sys.id,
     title: item.fields.title,
+    heroTitle: "Private " + item.fields.title + " Day Trip With Driver",
+    summaryTitle: "Our Private " + item.fields.title + " Excursion",
+    characterTitle: "Why Choose a Private " + item.fields.title + " Excursion?",
     slug: item.fields.slug,
     summary: item.fields.summary,
+
     heroImage: item.fields.heroImage?.fields?.file?.url || null,
-    photos: item.fields.photos?.map(
-      (p) => p.fields?.file?.url
-    ) || [],
+
+    photos: allPhotos.slice(0, 3),
+    photoWhy: allPhotos.slice(3),
+
+    characterTopLeft: characterImages[0] || null,
+    characterTopCenter: characterImages[1] || null,
+    characterBottomLeft: characterImages[2] || null,
+    characterBottomRight: characterImages[3] || null,
+
     halfDay: item.fields.halfDay,
     fullDay: item.fields.fullDay,
     footerTitle: item.fields.footerTitle,
